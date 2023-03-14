cada66700cae4cd6d009db34c9ece91bbc90a23e
Robert Dickerson <rob@robd.io>
2023-03-14 01:44:12 -0400

    Adding clap for arg parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Input file
+   #[arg(short, long)]
+   input: String,
+
+   /// Output a dot file representation of the e-graph
+   #[arg(short, long)]
+   dot: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }

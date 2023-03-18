c2cf3816ccd04f5071a972366f5a24ddb93d0912
Robert Dickerson <rob@robd.io>
2023-03-18 19:03:50 -0400

    MILP extraction without alignment discounts.

--- src/eggroll/extractor.rs
+++ src/eggroll/extractor.rs
@@ -6,66 +6,84 @@ use std::collections::HashMap;
 pub struct EggrollExtractor<'a> {
   egraph: &'a EGraph<Eggroll, ()>,
   model: Model,
-  vars: HashMap<Id, ClassVars>,
+  groups: HashMap<Id, ChoiceGroup>,
 }
 
-struct ClassVars {
-  class: Col,
-  nodes: Vec<Col>,
+struct Choice {
+  index: usize,
+  col: Col,
+  cost: f64,
+  required: Vec<Choice>,
+}
+
+struct ChoiceGroup {
+  col: Col,
+  choices: Vec<Choice>,
 }
 
 impl<'a> EggrollExtractor<'a> {
   pub fn new(egraph: &'a EGraph<Eggroll, ()>) -> Self {
     let mut model = Model::default();
 
-    let vars: HashMap<Id, ClassVars> = egraph
+    let groups: HashMap<Id, ChoiceGroup> = egraph
       .classes()
       .map(|class| {
-        let cvars = ClassVars {
-          class: model.add_binary(),
-          nodes: class.nodes.iter().map(|_| model.add_binary()).collect(),
-        };
-        (class.id, cvars)
+        let mut choices = Vec::new();
+        for (index, node) in class.nodes.iter().enumerate() {
+          let cost = match node {
+            Eggroll::Rel(_) => 0.0,
+            _ => 100.0,
+          };
+          choices.push(Choice { index,
+                                col: model.add_binary(),
+                                cost: cost,
+                                required: Vec::new() })
+        }
+        let cgroup = ChoiceGroup { col: model.add_binary(), choices };
+        (class.id, cgroup)
       })
       .collect();
 
-    for (&id, cvars) in &vars {
-      // One node in each selected class must get selected.
+    for (&id, group) in &groups {
+      // One node in each selected group must get selected.
       let select_node = model.add_row();
       model.set_row_equal(select_node, 0.0);
-      model.set_weight(select_node, cvars.class, -1.0);
-      for &node_active in &cvars.nodes {
-        model.set_weight(select_node, node_active, 1.0);
+      model.set_weight(select_node, group.col, -1.0);
+      for choice in &group.choices {
+        model.set_weight(select_node, choice.col, 1.0);
       }
 
       // All child classes of selected nodes must get selected.
-      for (_, (node, &node_var)) in egraph[id].iter().zip(&cvars.nodes).enumerate() {
+      for (_, (node, choice)) in egraph[id].iter().zip(&group.choices).enumerate() {
         for child in node.children() {
-          let child_var = vars[child].class;
+          let child_group = &groups[child];
           let select_child = model.add_row();
           model.set_row_upper(select_child, 0.0);
-          model.set_weight(select_child, node_var, 1.0);
-          model.set_weight(select_child, child_var, -1.0);
+          model.set_weight(select_child, choice.col, 1.0);
+          model.set_weight(select_child, child_group.col, -1.0);
         }
       }
     }
 
+    // Add low cost choices for good-looking alignments.
+    // TODO
+
     // Set model to minimize cost.
     model.set_obj_sense(Sense::Minimize);
     for class in egraph.classes() {
-      for (node, &node_var) in class.iter().zip(&vars[&class.id].nodes) {
-        model.set_obj_coeff(node_var, 1.0);//cost_function.cost(egraph, class.id, node));
+      for (_, choice) in class.iter().zip(&groups[&class.id].choices) {
+        model.set_obj_coeff(choice.col, choice.cost);
       }
     }
 
-    Self { egraph, model, vars }
+    Self { egraph, model, groups }
   }
 
   pub fn solve(&mut self, root: Id) -> RecExpr<Eggroll> {
     let egraph = self.egraph;
 
     let root = &egraph.find(root);
-    self.model.set_col_lower(self.vars[root].class, 1.0);
+    self.model.set_col_lower(self.groups[root].col, 1.0);
 
     let solution = self.model.solve();
     log::info!(
@@ -83,10 +101,12 @@ impl<'a> EggrollExtractor<'a> {
         todo.pop();
         continue;
       }
-      let cvars = &self.vars[&id];
-      assert!(solution.col(cvars.class) > 0.0);
-      let node_idx = cvars.nodes.iter().position(|&n| solution.col(n) > 0.0).unwrap();
-      let node = &self.egraph[id].nodes[node_idx];
+      let group = &self.groups[&id];
+      assert!(solution.col(group.col) > 0.0);
+      let choice_index = group.choices.iter()
+        .position(|choice| solution.col(choice.col) > 0.0)
+        .unwrap();
+      let node = &self.egraph[id].nodes[group.choices[choice_index].index];
       if node.all(|child| ids.contains_key(&child)) {
         let new_id = expr.add(node.clone().map_children(|i| ids[&self.egraph.find(i)]));
         ids.insert(id, new_id);

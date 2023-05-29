77fa7ad56594d0339d30afdda4b30066954e4dea
Rob Dickerson <rob@robd.io>
2023-05-28 22:19:04 -0400

    Moving an unno benchmark to new spec format.

--- benchmarks/unno/square-sum.c
+++ benchmarks/unno/square-sum.c
@@ -1,27 +1,22 @@
-int main(void) {
+/* @KESTREL
+ * pre:   left.a == right.a && left.b == right.b;
+ * left:  left;
+ * right: right;
+ * post:  left.c == right.c;
+ */
 
-  int a = arb_int();
-  int b = arb_int();
-
-  rel_left();
-
-  int a1 = a;
-  int b1 = b;
-  int c1 = 0;
-  while (a1 < b1) {
-   c1 = c1 + (a1 * a1);
-   a1 = a1 + 1;
+void left(int a, int b) {
+  int c = 0;
+  while (a < b) {
+   c = c + (a * a);
+   a = a + 1;
   }
+}
 
-  rel_mid();
-
-  int a2 = a;
-  int b2 = b;
-  int c2 = 0;
-  while (a2 < b2) {
-   c2 = c2 + (a2 * a2);
-   a2 = a2 + 1;
+void right(int a, int b) {
+  int c = 0;
+  while (a < b) {
+   c = c + (a * a);
+   a = a + 1;
   }
-
-  rel_right();
 }

4004484326633e34c9dc08ccec5acc5a5626ddf1
Prasita Mukherjee <54381461+prasitaGit@users.noreply.github.com>
2023-10-02 00:26:36 -0400

    Add files via upload

--- VerifiableC/Verif_barthe_tiny.v
+++ VerifiableC/Verif_barthe_tiny.v
@@ -108,9 +108,9 @@ Definition verifyfunca_spec : ident * funspec :=
       PROP (
       writable_share sh1;
       writable_share sh2;
-      Forall (fun x => 0 <= x + 1 <= Int.max_unsigned) contents_la;
-      Forall (fun x => 0 <= x <= Int.max_unsigned) contents_lb;
-      Forall2 (fun x y => 0 <= x + y + 1 <= Int.max_unsigned) contents_la contents_lb)
+      Forall (fun x => 0 <= x <= Int.max_unsigned) contents_la;
+      Forall (fun x => 0 <= x <= Int.max_unsigned) contents_lb)
+     (** Forall2 (fun x y => 0 <= x + y + 1 <= Int.max_unsigned) contents_la contents_lb)*)
     PARAMS (l_a; l_b)
       SEP (data_at sh1 (tarray tuint 100) (map Vint (map Int.repr contents_la)) l_a;
       data_at sh2 (tarray tuint 100) (map Vint (map Int.repr contents_lb)) l_b)
@@ -118,13 +118,36 @@ Definition verifyfunca_spec : ident * funspec :=
       PROP ()
       RETURN () (*void*)
       SEP (data_at sh1 (tarray tuint 100) (map Vint (map Int.repr (sum_lZ (sublist 0 100 contents_la)))) l_a;
-      data_at sh2 (tarray tuint 100) (map Vint (map Int.repr (sum_aZ (sublist 0 100 contents_lb) (sublist 0 100 contents_la)))) l_b).    
+      data_at sh2 (tarray tuint 100) (map Vint (map Int.repr (sum_aZ (sublist 0 100 contents_lb) (sum_lZ (sublist 0 100 contents_la))))) l_b).    
 
+(*API spec => verifyfuncc spec - the entire pipeline setup *)
+Definition verifyfuncc_spec : ident * funspec :=
+  DECLARE _verifyfuncc
+    WITH l_a : val, l_b : val, l_c : val, sh1 : share, sh2 : share, sh3 : share,
+    contents_la : list Z, contents_lb : list Z, contents_lc : list Z
+    PRE [tptr tuint, tptr tuint, tptr tuint]  
+      (*ensure variables left and right are equal*)
+      PROP (
+      writable_share sh1;
+      writable_share sh2;
+      writable_share sh3;
+      Forall (fun x => 0 <= x + 1 <= Int.max_unsigned) contents_la;
+      Forall (fun x => 0 <= x <= Int.max_unsigned) contents_lb;
+      Forall (fun x => 0 <= x <= Int.max_unsigned) contents_lc)
+      (*Forall2 (fun x y => 0 <= x + y + 1 <= Int.max_unsigned) contents_la contents_lb)*)
+    PARAMS (l_a; l_b; l_c)
+      SEP (data_at sh1 (tarray tuint 100) (map Vint (map Int.repr contents_la)) l_a;
+      data_at sh2 (tarray tuint 100) (map Vint (map Int.repr contents_lb)) l_b;
+      data_at sh3 (tarray tuint 100) (map Vint (map Int.repr contents_lc)) l_c)
+    POST [ tvoid ]
+      PROP ()
+      RETURN () (*void*)
+      SEP (data_at sh1 (tarray tuint 100) (map Vint (map Int.repr (sum_lZ (sublist 0 100 contents_la)))) l_a;
+      data_at sh2 (tarray tuint 100) (map Vint (map Int.repr (sum_aZ (sublist 0 100 contents_lb) (sum_lZ (sublist 0 100 contents_la))))) l_b;
+      data_at sh3 (tarray tuint 100) (map Vint (map Int.repr (sum_aZ (sublist 0 100 contents_lc) (sum_aZ (sublist 0 100 contents_lb) (sum_lZ (sublist 0 100 contents_la)))))) l_c).   
 
 (*Pack APIs together*)
-Definition Gprog := [verifyfunc_spec; verifyfunca_spec].
-
-(*verify fun_spec - from here*)
+Definition Gprog := [verifyfunc_spec; verifyfunca_spec; verifyfuncc_spec].
 
 (*verify fun_spec - from here*)
 Lemma body_verifyfunc: semax_body Vprog Gprog f_verifyfunc verifyfunc_spec.
@@ -242,10 +265,10 @@ Proof.
  start_function. 
  (*a1 length is M*)
  assert_PROP (Zlength contents_la = 100). {
-  entailer!. rewrite <- H3. do 2 rewrite Zlength_map. reflexivity.
+  entailer!. rewrite <- H2. do 2 rewrite Zlength_map. reflexivity.
  }
  assert_PROP (Zlength contents_lb = 100). {
-  entailer!. rewrite <- H7. do 2 rewrite Zlength_map. reflexivity.
+  entailer!. rewrite <- H6. do 2 rewrite Zlength_map. reflexivity.
  }
  forward.
 forward_loop 
@@ -258,7 +281,7 @@ forward_loop
    SEP (data_at sh1 (tarray tuint 100) 
    (map Vint (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_la)))) l_a;
    data_at sh2 (tarray tuint 100) 
-   (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_lb)))) l_b))%assert
+   (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++ (sublist l_i 100 contents_lb)))) l_b))%assert
    break: 
    (EX l_i:Z, 
    PROP (0 <= l_i <= 100; l_i = 100)
@@ -268,10 +291,10 @@ forward_loop
     SEP (data_at sh1 (tarray tuint 100) 
     (map Vint (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_la)))) l_a;
     data_at sh2 (tarray tuint 100) 
-    (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_lb)))) l_b))%assert.
+    (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_lb))))) l_b))%assert.
  (*first existential - outermost if*)      
  Exists 0.  entailer!. simpl. rewrite !sublist_same_gen.
- entailer!. reflexivity. rewrite H3. lia. reflexivity. lia.
+ entailer!. reflexivity. lia. reflexivity. lia.
  Intros l_i. forward_if. forward. 
  (*second if - entailer*)
  Exists l_i.  entailer!. (*entailer!.*) 
@@ -289,32 +312,28 @@ forward_loop
   }
   assert_PROP (0 <= l_i < Zlength ((sum_lZ (sublist 0 l_i contents_la)) ++
  sublist l_i 100 contents_la)). {
-  entailer!. rewrite Zlength_map in H7. assumption.
+  entailer!. rewrite Zlength_map in H6. assumption.
   }
   (*for sublist b*)
-  assert_PROP (Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb)(sublist 0 l_i contents_la)) ++
+  assert_PROP (Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
  sublist l_i 100 contents_lb)) = 100). {
   entailer!. rewrite Zlength_map.
   rewrite Zlength_app. rewrite <- samea_listLength.
   rewrite <- Zlength_app. rewrite sublist_rejoin with 0 l_i 100 contents_lb.
-  rewrite sublist_same_gen. assumption. reflexivity. 
-  lia. lia. lia. Search Zlength sublist 0. 
-  rewrite Zlength_sublist with 0 l_i contents_lb. rewrite Zlength_sublist with 0 l_i contents_la.
-  reflexivity. lia. lia. lia. lia.
+  Check Zlength_sublist. rewrite Zlength_sublist. reflexivity.
+  lia. lia. lia. lia. rewrite <- same_listLength by lia. 
+  rewrite !Zlength_sublist by lia. reflexivity.
   }
-  assert_PROP (0 <= l_i < Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb)(sublist 0 l_i contents_la)) ++
+  assert_PROP (0 <= l_i < Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
   sublist l_i 100 contents_lb))). {
   entailer!. 
   }
-  assert_PROP (0 <= l_i < Zlength ((sum_aZ (sublist 0 l_i contents_lb)(sublist 0 l_i contents_la)) ++
+  assert_PROP (0 <= l_i < Zlength ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
   sublist l_i 100 contents_lb)). {
-  entailer!. rewrite Zlength_map in H10. assumption.
+  entailer!. rewrite Zlength_map in H9. assumption.
   }
-  forward. forward. forward. forward. Print local. Print mpred.
-  Print environ. Print tc_val.
-  
-  Print Vint. Print liftx.
-  Print Lift.  list_simplify. forward. forward. Exists (l_i + 1).
+  forward. forward. forward. forward. 
+  list_simplify. forward. forward. Exists (l_i + 1).
   entailer!. apply sepcon_derives.  do 2 rewrite upd_Znth_map.  
   rewrite (sublist_split 0 l_i (l_i+1)). rewrite (sublist_split l_i (l_i+1) 100).
   eapply add_entailment. lia. assumption. lia. lia. lia. lia.
@@ -324,20 +343,227 @@ forward_loop
   Search force_val Some. rewrite force_val_e.
   Print both_int. unfold both_int. simpl.
   rewrite sem_cast_i2i_correct_range. 2:{ list_simplify. } 
-  simpl. list_simplify. rewrite force_val_e. rewrite <- !samea_listLength.
+  simpl. try list_solve. list_simplify. rewrite force_val_e. rewrite <- !samea_listLength.
   rewrite <- !same_listLength.  rewrite !Zlength_sublist. 
   replace (l_i - 0) with l_i by lia. replace (l_i - l_i + l_i) with l_i by lia.
   rewrite !upd_Znth_map. f_equal. 
   rewrite add_repr.
-  rewrite !upd_Znth_map. f_equal. 
+  rewrite !upd_Znth_map. f_equal. Print list_simplify.
   rewrite !upd_Znth_app2. 
   rewrite <- !samea_listLength. rewrite !Zlength_sublist. 
   replace (l_i - (l_i - 0)) with 0 by lia. 
   rewrite (sublist_split l_i (l_i+1) 100). rewrite sublist_len_1. simpl.   
   rewrite upd_Znth0. rewrite app_cons_assoc. f_equal.
   rewrite !(sublist_split 0 l_i (l_i+1)). Search sublist. 
-  rewrite !sublist_len_1. unfold sum_aZ at 2.
+  rewrite !sublist_len_1. unfold sum_lZ. Search map app.
+  rewrite map_last. Search Z.succ 1. rewrite Z.add_1_r. 
+  unfold sum_aZ at 2.
   Search combine app. rewrite combine_app'. simpl.
   Search map app. rewrite map_app. simpl. reflexivity.
-  
-Admitted.
+  Search Zlength map. rewrite Zlength_map.
+  rewrite !Zlength_sublist by lia. reflexivity.
+  lia. lia. lia. lia. lia. lia. lia. lia. lia. lia. lia.
+  rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- !samea_listLength.
+  rewrite !Zlength_sublist by lia. lia.
+  rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. lia. lia. lia. lia. 
+  rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity.
+  Intros l_i. rewrite H4. entailer!.
+  apply sepcon_derives. 
+  apply derives_refl'. f_equal. f_equal. f_equal.
+  Search sublist nil. rewrite sublist_nil.
+  Search app nil. rewrite <- app_nil_end. reflexivity.
+  apply derives_refl'. f_equal. f_equal. f_equal.
+  rewrite sublist_nil. rewrite <- app_nil_end. reflexivity.
+Qed.
+
+(*verify fun_spec c - all three arrays*)
+Lemma body_verifyfuncc: semax_body Vprog Gprog f_verifyfuncc verifyfuncc_spec.
+Proof. 
+ start_function. 
+ (*a1 length is M*)
+ assert_PROP (Zlength contents_la = 100). {
+  entailer!. rewrite <- H3. do 2 rewrite Zlength_map. reflexivity.
+ }
+ assert_PROP (Zlength contents_lb = 100). {
+  entailer!. rewrite <- H7. do 2 rewrite Zlength_map. reflexivity.
+ }
+ assert_PROP (Zlength contents_lc = 100). {
+  entailer!. rewrite <- H11. do 2 rewrite Zlength_map. reflexivity.
+ }
+ forward.
+forward_loop 
+  (EX l_i:Z,  
+    PROP (0 <= l_i <= 100)
+   LOCAL (
+    temp _l_i (Vint (Int.repr l_i)); 
+    temp _l_a l_a; temp _l_b l_b; temp _l_c l_c
+    )(*map.Vint (map Int.repr (sublist 0 l_x contents_f ++ sublist l_x (N * M) contents a_1)) a_1*)
+   SEP (data_at sh1 (tarray tuint 100) 
+   (map Vint (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_la)))) l_a;
+   data_at sh2 (tarray tuint 100) 
+   (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++ (sublist l_i 100 contents_lb)))) l_b;
+   data_at sh3 (tarray tuint 100) 
+   (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lc) (sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)))) ++ (sublist l_i 100 contents_lc)))) l_c
+   ))%assert
+   break: 
+   (EX l_i:Z, 
+   PROP (0 <= l_i <= 100; l_i = 100)
+   LOCAL (
+    temp _l_i (Vint (Int.repr l_i)); 
+    temp _l_a l_a; temp _l_b l_b; temp _l_c l_c)
+    SEP (data_at sh1 (tarray tuint 100) 
+    (map Vint (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_la)))) l_a;
+    data_at sh2 (tarray tuint 100) 
+    (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)) ++ (sublist l_i 100 contents_lb))))) l_b;
+    data_at sh3 (tarray tuint 100) 
+    (map Vint (map Int.repr ((sum_aZ (sublist 0 l_i contents_lc) (sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)))) ++ (sublist l_i 100 contents_lc)))) l_c))%assert.
+ (*first existential - outermost if*)      
+ Exists 0.  entailer!. simpl. rewrite !sublist_same_gen by lia.
+ entailer!. 
+ Intros l_i. forward_if. forward. 
+ (*second if - entailer*)
+ Exists l_i.  entailer!. (*entailer!.*) 
+ assert_PROP (Zlength (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++
+ sublist l_i 100 contents_la)) = 100). {
+  entailer!. rewrite Zlength_map.
+  rewrite Zlength_app. rewrite <- same_listLength.
+  rewrite <- Zlength_app. rewrite sublist_rejoin with 0 l_i 100 contents_la.
+  rewrite sublist_same_gen. assumption. reflexivity. 
+  lia. lia. lia.
+  }
+  assert_PROP (0 <= l_i < Zlength (map Int.repr ((sum_lZ (sublist 0 l_i contents_la)) ++
+ sublist l_i 100 contents_la))). {
+  entailer!. 
+  }
+  assert_PROP (0 <= l_i < Zlength ((sum_lZ (sublist 0 l_i contents_la)) ++
+ sublist l_i 100 contents_la)). {
+  entailer!. rewrite Zlength_map in H8. assumption.
+  }
+  (*for sublist b*)
+  assert_PROP (Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
+ sublist l_i 100 contents_lb)) = 100). {
+  entailer!. rewrite Zlength_map.
+  rewrite Zlength_app. rewrite <- samea_listLength.
+  rewrite <- Zlength_app. rewrite sublist_rejoin with 0 l_i 100 contents_lb.
+  rewrite Zlength_sublist. reflexivity.
+  lia. lia. lia. lia. rewrite <- same_listLength by lia. 
+  rewrite !Zlength_sublist by lia. reflexivity.
+  }
+  assert_PROP (0 <= l_i < Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
+  sublist l_i 100 contents_lb))). {
+  entailer!. 
+  }
+  assert_PROP (0 <= l_i < Zlength ((sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la))) ++
+  sublist l_i 100 contents_lb)). {
+  entailer!. rewrite Zlength_map in H11. assumption.
+  }
+  (*for sublist c*)
+  assert_PROP (Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lc) (sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)))) ++
+    sublist l_i 100 contents_lc)) = 100). {
+     entailer!. rewrite Zlength_map.
+     rewrite Zlength_app. rewrite <- samea_listLength.
+     rewrite <- Zlength_app. rewrite sublist_rejoin with 0 l_i 100 contents_lc.
+     rewrite Zlength_sublist by lia. reflexivity.
+     lia. lia. 
+     rewrite <- samea_listLength. rewrite !Zlength_sublist by lia. reflexivity. 
+     rewrite <- same_listLength by lia. 
+     rewrite !Zlength_sublist by lia. reflexivity.
+  }
+  assert_PROP (0 <= l_i < Zlength (map Int.repr ((sum_aZ (sublist 0 l_i contents_lc) (sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)))) ++
+     sublist l_i 100 contents_lc))). {
+     entailer!. 
+  }
+  assert_PROP (0 <= l_i < Zlength ((sum_aZ (sublist 0 l_i contents_lc) (sum_aZ (sublist 0 l_i contents_lb) (sum_lZ (sublist 0 l_i contents_la)))) ++
+     sublist l_i 100 contents_lc)). {
+     entailer!. rewrite Zlength_map in H14. assumption.
+  }
+  forward. forward. forward. forward. 
+  list_simplify. forward. forward. forward. list_simplify.
+  forward. forward. Exists (l_i + 1). entailer!.
+  apply sepcon_derives. apply sepcon_derives. 
+  (*sublist a*)
+  do 2 rewrite upd_Znth_map.  
+  rewrite (sublist_split 0 l_i (l_i+1)). rewrite (sublist_split l_i (l_i+1) 100).
+  eapply add_entailment. lia. assumption. lia. lia. lia. lia.
+  (*sublist b*)
+  apply derives_refl'. f_equal.
+  rewrite sem_cast_i2i_correct_range. 
+  2:{ simpl. list_simplify. }
+  Search force_val Some. rewrite force_val_e.
+  Print both_int. unfold both_int. simpl.
+  rewrite sem_cast_i2i_correct_range. 2:{ list_simplify. } 
+  simpl. try list_solve. list_simplify. rewrite force_val_e. rewrite <- !samea_listLength.
+  rewrite <- !same_listLength by lia.  rewrite !Zlength_sublist by lia. 
+  replace (l_i - 0) with l_i by lia. replace (l_i - l_i + l_i) with l_i by lia.
+  rewrite !upd_Znth_map by lia. f_equal. 
+  rewrite add_repr.
+  rewrite !upd_Znth_map. f_equal. Print list_simplify.
+  rewrite !upd_Znth_app2. 
+  rewrite <- !samea_listLength. rewrite !Zlength_sublist by lia. 
+  replace (l_i - (l_i - 0)) with 0 by lia. 
+  rewrite (sublist_split l_i (l_i+1) 100). rewrite sublist_len_1. simpl.   
+  rewrite upd_Znth0. rewrite app_cons_assoc. f_equal.
+  rewrite !(sublist_split 0 l_i (l_i+1)). Search sublist. 
+  rewrite !sublist_len_1. unfold sum_lZ. 
+  rewrite map_last.  rewrite Z.add_1_r. 
+  unfold sum_aZ at 2. rewrite combine_app'. simpl.
+  rewrite map_app. simpl. reflexivity.
+  rewrite Zlength_map.
+  rewrite !Zlength_sublist by lia. reflexivity.
+  lia. lia. lia. lia. lia. lia. lia. lia. lia. 
+  rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- !samea_listLength. 
+  rewrite !Zlength_sublist by lia. lia.
+  rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity.
+  (*sublist c*)
+  apply derives_refl'. f_equal.
+  rewrite !sem_cast_i2i_correct_range. 
+  2:{ simpl. list_simplify. }
+  2:{ simpl. list_simplify. }
+  rewrite !force_val_e.
+  unfold both_int. simpl.
+  rewrite !sem_cast_i2i_correct_range. 2:{ list_simplify. } 2:{ list_simplify. } 
+  simpl. try list_solve. list_simplify. rewrite !force_val_e. rewrite <- !samea_listLength.
+  rewrite <- !same_listLength by lia.  rewrite !Zlength_sublist by lia. 
+  replace (l_i - 0) with l_i by lia. replace (l_i - l_i + l_i) with l_i by lia.
+  rewrite !upd_Znth_map by lia. f_equal. 
+  rewrite !add_repr.
+  rewrite !upd_Znth_map. f_equal. rewrite !upd_Znth_app2. 
+  rewrite <- !samea_listLength. rewrite !Zlength_sublist by lia. 
+  replace (l_i - (l_i - 0)) with 0 by lia. Search upd_Znth 0.
+  rewrite !(sublist_split l_i (l_i+1) 100). rewrite !sublist_len_1. simpl.   
+  rewrite upd_Znth0. rewrite app_cons_assoc. f_equal.
+  rewrite !(sublist_split 0 l_i (l_i+1)). 
+  rewrite !sublist_len_1. 
+  unfold sum_lZ. rewrite map_last.  rewrite Z.add_1_r. 
+  unfold sum_aZ at 4. rewrite combine_app'. simpl. 
+  unfold sum_aZ at 3. simpl. rewrite combine_map_snd.
+  rewrite combine_app'. simpl. Check map_app.
+  rewrite map_app. simpl. rewrite map_app. simpl.
+  unfold sum_aZ at 2. unfold sum_aZ at 1. rewrite combine_map_snd.
+  reflexivity.
+  simpl. Search Zlength combine. rewrite Zlength_combine.
+  Search Zlength map. rewrite Zlength_map. rewrite !Zlength_sublist by lia.
+  replace (l_i - 0) with l_i by lia. Search Z.min. rewrite Z.min_id. reflexivity.
+  (*second case same*)
+  simpl.  rewrite Zlength_map. rewrite !Zlength_sublist by lia. reflexivity.
+  lia. lia. lia. lia. lia. lia. lia. lia. lia. lia. lia. lia.
+  rewrite <- samea_listLength. rewrite !Zlength_sublist by lia. reflexivity.
+  rewrite <- !same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity.
+  (*Final <=*)
+  rewrite <- !samea_listLength. rewrite !Zlength_sublist by lia.
+  lia. rewrite <- samea_listLength. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- same_listLength by lia. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- samea_listLength. rewrite !Zlength_sublist by lia.
+  reflexivity. rewrite <- same_listLength. rewrite !Zlength_sublist by lia. reflexivity.
+  (*Final*)
+  Intros l_i. rewrite H6. rewrite !sublist_nil.
+  rewrite <- !app_nil_end. entailer!.
+Qed.

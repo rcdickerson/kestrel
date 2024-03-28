68772dc73955dfcaadeadadf26bb679af0b0f7c9
Rob Dickerson <rob@robd.io>
2024-03-28 18:34:09 -0400

    Adding some axiomatization to list array sum benchmark.

--- benchmarks/euf/adt/list-array-sum.c
+++ benchmarks/euf/adt/list-array-sum.c
@@ -1,8 +1,15 @@
 /* @KESTREL
- * pre: list_val(left.list_in) == read(right.arr_in, 0);
+ * pre: (forall list: int :: r_advance(list, list, 0) == 1)
+     && (forall l1: int, l2: int :: l1 != l2 ==> r_advance(l1, l2, 0) == 0)
+     && (forall l1: int, l2: int, l2next: int, n: int ::
+             (l2next == list_next(l2) && r_advance(l1, l2, n) == 1)
+             ==> r_advance(l1, l2next, n + 1) == 1)
+     && (forall list: int, n: int ::
+             r_advance(left.list_in, list, n) == 1
+              ==> list_val(list) == read(right.arr_in, n));
  * left: sum_list;
  * right: sum_array;
- * post: left.sum <= right.sum;
+ * post: left.sum == right.sum;
  */
 
 // List interface.
@@ -13,6 +20,10 @@ int list_null(int list);
 // Array interface.
 int read(int list_id, int index);
 int store(int list_id, int index, int value);
+
+// A relation indicating that list l2 corresponds to list l1 after n
+// advancements.
+int r_advance(int l1, int l2, int n);
 
 void _test_gen(int arr, int list, int length) {
   if (length < 0) { length = length * -1; }

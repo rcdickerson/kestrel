de73c3874588705bc500aea171525a5211e786d8
Robert Dickerson <rob@robd.io>
2025-04-18 16:31:58 -0400

    Adding param usage benchmarks.

---
+++ benchmarks/oriole/param-usage/coin-unused.c
@@ -0,0 +1,30 @@
+/* @ELAENIA
+ * pre: forall.param1A == exists.param1A
+     && forall.param2A != exists.param2A
+     && forall.param1B != exists.param2B
+     && forall.param2B == exists.param2B;
+ * forall: unused;
+ * exists: unused;
+ * post: forall.rA == exists.rA
+      || forall.rB == exists.rB;
+ * aspecs: flip() {
+ *     pre:  true;
+ *     post: ret! == 0 || ret! == 1;
+ * }
+ * especs:
+ *   randInt() {
+ *     choiceVars: n;
+ *     pre:  n == 0 || n == 1;
+ *     post: ret! == n;
+ * }
+ */
+
+int flip();
+
+void unused(int param1A, int param2A, int param1B, int param2B) {
+  int rA;
+  rA = flip();
+
+  int rB;
+  rB = flip();
+}
---
+++ benchmarks/oriole/param-usage/nondet-unused.c
@@ -0,0 +1,44 @@
+/* @ELAENIA
+ * pre: forall.param1A == exists.param1A
+     && forall.param2A != exists.param2A
+     && forall.param1B != exists.param1B
+     && forall.param2B == exists.param2B;
+ * forall: unused;
+ * exists: unused;
+ * post: forall.retA == exists.retA
+      || forall.retB == exists.retB;
+ * aspecs: flip() {
+ *     pre:  true;
+ *     post: ret! == 0 || ret! == 1;
+ * }
+ * especs:
+ *   randInt() {
+ *     choiceVars: n;
+ *     pre:  n == 0 || n == 1;
+ *     post: ret! == n;
+ * }
+ */
+
+int flip();
+
+void unused(int param1A, int param2A, int param1B, int param2B) {
+  int xA = param1A + param2A;
+  int rA;
+  rA = flip();
+  int retA;
+  if (rA == 0) {
+    retA = xA - param2A;
+  } else {
+    retA = param1A;
+  }
+
+  int xB = param1B + param2B;
+  int rB;
+  rB = flip();
+  int retB;
+  if (rB == 0) {
+    retB = xB - param2B;
+  } else {
+    retB = param1B;
+  }
+}

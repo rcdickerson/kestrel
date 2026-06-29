82050014c53f8f01e06a69d6bc92eca3d69f9dee
lucas <lucasrettig23@augustana.edu>
2026-06-29 17:36:46 -0500

    Adding initial LLVM to C routing stub.

--- src/main.rs
+++ src/main.rs
@@ -157,6 +157,15 @@ fn setup_working_dir() -> Result<(), std::io::Error> {
 /// non-relational and relational programs, and 2) packaging programs
 /// into an Egg-compatible language definition.
 fn kestrel_workflow(args: Args) {
+
+  // --- LLVM-IR Pre-Processing ---
+  if args.input.ends_with(".ll") {
+    kestrel::crel::llvm_to_c::process_llvm_file(&args.input);
+    println!("Exiting early: LLVM pipeline is not yet fully connected.");
+    return;
+  }
+
+
   let mut raw_crel = kestrel::crel::parser::parse_c_file(&args.input);
   if args.extractor == ExtractorArg::Unaligned {
     // Annotated invariants are relational.
